cdac804ba874271d165bd15e0d2783c8cae2df00
Matthew Holman <mholman000@gmail.com>
2026-02-02 16:57:09 +0100

    Remove router

--- app/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST(request: Request) {
-  const url = new URL(request.url);
-  url.searchParams.set("sent", "1");
-  url.hash = "contact";
-  return NextResponse.redirect(url, 303);
-}
